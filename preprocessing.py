f337d849ba839b3fd008450d1c1ffaed961deb40
root <tranducmanh12092001@gmail.com>
2022-09-22 10:51:00 +0000

    More Precessing Data

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,6 @@
 import unicodedata
 import re
+from language import Language
 
 # Lowercase, trim, and remove non-letter characters
 def unicode_to_ascii(string):
@@ -15,4 +16,21 @@ def normalize_string(string):
     return string
 
 def read_languages(language1, language2, reverse = False):
-    pass
+    print("Reading lines...")
+
+    # Read the file and split into lines
+    lines = open('data/%s-%s.txt' % (language1, language2), encoding='utf-8').read().strip().split('\n')
+
+    # Split every line into pairs and normalize
+    pairs = [[normalize_string(string) for string in line.split('\t')] for line in lines]
+
+    # Reverse pairs, make Language instances
+    if reverse:
+        pairs = [list(reversed(pair)) for pair in pairs]
+        input_language = Language(language1)
+        output_language = Language(language2)
+    else:
+        input_language = Language(language1)
+        output_language = Language(language2)
+
+    return input_language, output_language, pairs
