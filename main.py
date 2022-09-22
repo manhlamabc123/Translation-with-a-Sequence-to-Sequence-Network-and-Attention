b8bbe1a4aa8bb57d831830881a221feaea8fdbdb
root <tranducmanh12092001@gmail.com>
2022-09-22 11:10:01 +0000

    Preprocessing Data

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ import torch
 import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
+from preprocessing import *
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+#Preprocessing Data
+input_language, output_language, pairs = prepare_data('eng', 'fra', True)
