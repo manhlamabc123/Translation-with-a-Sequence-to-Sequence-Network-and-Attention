b2151ed81d54736be51693792259fd4ef1d0aa59
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-09-23 14:21:03 +0700

    Clean code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from evalute.evalute import evaluate_randomly
 
 from preprocessing.preprocessing import prepare_data
 from model.encoder import EncoderRNN
-from constants import HIDDEN_SIZE, device
+from constants import DROPOUT_PROBABILITY, HIDDEN_SIZE, N_ITERS, PRINT_EVERY, device
 from model.attention_decoder import AttentionDecoderRNN
 from train.train import train_iters
 
@@ -12,8 +12,8 @@ input_language, output_language, pairs = prepare_data('eng', 'fra', True)
 
 # Training and Evaluating
 encoder1 = EncoderRNN(input_language.n_words, HIDDEN_SIZE).to(device)
-attention_decoder1 = AttentionDecoderRNN(HIDDEN_SIZE, output_language.n_words, dropout_probability=0.1).to(device)
+attention_decoder1 = AttentionDecoderRNN(HIDDEN_SIZE, output_language.n_words, dropout_probability=DROPOUT_PROBABILITY).to(device)
 
-train_iters(input_language, output_language, pairs, encoder1, attention_decoder1, 75000, print_every=5000)
+train_iters(input_language, output_language, pairs, encoder1, attention_decoder1, N_ITERS, print_every=PRINT_EVERY)
 
 evaluate_randomly(input_language, output_language, pairs, encoder1, attention_decoder1)
